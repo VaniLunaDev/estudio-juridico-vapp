22ed29a10fe9023755917a4107c72277c61f6db5
VaninaLuna <vani.lunaa@gmail.com>
2026-02-21 10:50:06 -0300

    update image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -16,65 +16,94 @@ export default async function Image() {
           width: "100%",
           height: "100%",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#f5f0eb",
-          fontFamily: "system-ui, sans-serif",
+          backgroundColor: "#F2EBE3",
+          padding: "50px",
+          fontFamily: "sans-serif",
         }}
       >
         <div
           style={{
             display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: 48,
-            padding: 80,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#1A1A17",
+            borderRadius: "32px",
+            overflow: "hidden",
+            position: "relative",
           }}
         >
-          <img
-            src={`${baseUrl}/favicon.svg`}
-            width={200}
-            height={200}
-            alt=""
-            style={{ objectFit: "contain" }}
-          />
+          {/* Lado izquierdo: Toque de color */}
+          <div style={{ width: "20px", height: "100%", backgroundColor: "#B49A78" }} />
+
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: 12,
-              maxWidth: 600,
+              justifyContent: "center",
+              padding: "0 80px",
+              flex: 1,
             }}
           >
             <div
               style={{
-                fontSize: 56,
-                fontWeight: 700,
-                color: "#4a4035",
-                lineHeight: 1.2,
+                fontSize: "20px",
+                color: "#B49A78",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                letterSpacing: "4px",
+                marginBottom: "20px",
               }}
             >
-              Pozzatto y Pezzutti Asociados
+              Estudio Jurídico • Mendoza
             </div>
+
             <div
               style={{
-                fontSize: 32,
-                color: "#87734d",
-                fontWeight: 600,
+                fontSize: "72px",
+                fontWeight: "bold",
+                color: "#FFFFFF",
+                lineHeight: 1.1,
+                marginBottom: "30px",
               }}
             >
-              Estudio Jurídico en Mendoza
+              Pozzatto & Pezzutti <br />
+              <span style={{ color: "#B49A78" }}>Asociados</span>
             </div>
+
+            <div style={{ width: "80px", height: "4px", backgroundColor: "#B49A78", marginBottom: "30px" }} />
+
             <div
               style={{
-                fontSize: 24,
-                color: "#6b5d4f",
-                marginTop: 8,
+                fontSize: "28px",
+                color: "#D1D1D1",
+                maxWidth: "700px",
+                lineHeight: 1.4,
+                fontStyle: "italic"
               }}
             >
-              Asesoramiento en derecho societario, laboral, MASC, ejecuciones, contratos e inmobiliario.
+              &quot;Excelencia legal con un toque personal&quot;
             </div>
+          </div>
+
+          <div
+            style={{
+              position: "absolute",
+              right: "60px",
+              bottom: "60px",
+              display: "flex",
+              alignItems: "center",
+              gap: "15px",
+              opacity: 0.8
+            }}
+          >
+            <img
+              src={`${baseUrl}/favicon.svg`}
+              width="30"
+              height="30"
+              style={{ filter: "brightness(0) saturate(100%) invert(71%) sepia(13%) saturate(763%) hue-rotate(3deg) brightness(91%) contrast(85%)" }}
+            />
+            <div style={{ width: "1px", height: "30px", backgroundColor: "#B49A78" }} />
+            <div style={{ color: "#FFFFFF", fontSize: "16px", letterSpacing: "1px" }}>SOLUCIONES INTEGRALES</div>
           </div>
         </div>
       </div>
